e295df2cbd8d2441b05f29e798778bae47d8250b
Vinicius Guilherme Batista <62727555+Vini-Dev-Py@users.noreply.github.com>
2021-03-15 12:05:13 -0300

    Docker

--- django/app/settings.py
+++ django/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'postgresql',
         'HOST': 'db',
         'PORT': 5432,
     }
